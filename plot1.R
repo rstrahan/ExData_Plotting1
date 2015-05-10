808ce812067b5f8a35a189c24421939eb2c01479
Bob Strahan <bob.strahan@gmail.com>
2015-05-10 10:33:41 -0400

    commit assignment code and images

---
+++ plot1.R
@@ -0,0 +1,44 @@
+library(data.table)
+library(lubridate)
+
+prepareData <- function() {
+    # set locations
+    url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    zipfile <- "./household_power_consumption.zip"
+    datafile <- "./household_power_consumption.txt"
+    # download and decompress the data file only if not already in the working directory
+    if (! file.exists(datafile)) {
+        if (! file.exists(zipfile)) {
+            print("Downloading data zip file")
+            download.file(url,zipfile, method="curl")
+        }
+        print("Unzipping data file")
+        unzip(zipfile)
+    }
+    # read the file into a data table
+    print("Loading data")
+    data <- read.csv(datafile, sep=";", na.strings=c("?"))
+    print("Preparing data")
+    # Convert Date column from character to POSIXct class - parse as date/month/year
+    data$Date = dmy(data$Date)
+    # subset only the dates we care about - 2007-02-01 and 2007-02-02
+    data <- subset(data, Date >= ymd("2007-02-01") & Date <= ymd("2007-02-02"))
+    # Convert Time column from character to POSIXct class
+    data$Time = hms(data$Time)
+    # Combine Date and Time
+    data$DateTime = data$Date + data$Time
+    return(data)
+}
+
+data <- prepareData()
+
+# create png graphics device
+file <- "plot1.png"
+cat("Creating PNG file: ",file)
+png(filename=file, width=480, height=480, units="px")
+hist(data$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
+print("Done")
+
+
